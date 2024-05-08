02724fedd8dbe81e40dc6b2e3a2dbc42e1740c37
KRISHNA <121825759+krishnaser@users.noreply.github.com>
2024-05-08 14:48:53 +0530

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
-  SESSION_NAME: process.env.SESSION_NAME || "Bot~"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://krishnadied:<krishnadied07lezya>@cluster0.kqbahwp.mongodb.net/",
+  SESSION_NAME: process.env.SESSION_NAME || "01-l-e-z-y-a-m-w-o-l"
 };
